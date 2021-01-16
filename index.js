d2d0311b644a9a05a7f7a0d531429456404e5c9d
tal kot <talkot@tals-MacBook-Pro.local>
2021-01-16 13:05:09 +0200

    Second commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   let data = {a: 30, b : {t:"tal", "kot": true}};
   res.json(data)
 })
+
+console.log("server starting..");
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
