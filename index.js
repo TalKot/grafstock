e04927f5c630af0994c93d7f62b05981f9e486d8
tal kot <talkot@tals-MacBook-Pro.local>
2021-01-16 14:04:36 +0200

    arkk data

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
+const request = require("request");
+const {StringStream} = require("scramjet");
+
 const port = 8080
+const arkCSV = "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"
+
+const app = express()
 
 app.get('/', (req, res) => {
-  let data = {a: 30, b : {t:"tal", "kot": true}};
-  res.json(data)
+  // let data = await request.get(arkCSV);
+  let data = [];
+
+  request.get(arkCSV)   // fetch csv
+  .pipe(new StringStream())                       // pass to stream
+  .CSVParse()                                   // parse into objects
+  .consume(object => data.push(object))  // do whatever you like with the objects
+  .then(() => res.json(data))
+  .catch(err => console.error(err))
 })
 
 console.log("server starting..");
