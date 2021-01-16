e1d896fbebd89c0a5672709fe15c114ca465ee6c
tal kot <talkot@tals-MacBook-Pro.local>
2021-01-16 18:29:22 +0200

    formatter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const request = require("request");
-const {StringStream} = require("scramjet");
+const {
+  StringStream
+} = require("scramjet");
 
 const port = 8080
 const arkCSV = "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"
@@ -8,31 +10,27 @@ const arkCSV = "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNO
 const app = express()
 
 app.get('/', async (req, res) => {
-  
-    let data = [];
-    
-    await request.get(arkCSV)  // fetch csv
-    .pipe(new StringStream())  // pass to stream
-    .CSVParse()                // parse into objects
+
+  let data = [];
+
+  await request.get(arkCSV) // fetch csv
+    .pipe(new StringStream()) // pass to stream
+    .CSVParse() // parse into objects
     .consume(object => {
       data.push(object);
     })
 
-    let colums = data.shift();
-    
-    let finalData = data.map(row => {
+  let colums = data.shift();
 
-      return row.reduce(function(result, field, index) {
-        result[colums[index]] = row[index];
-        return result;
-      }, 
-      {})
-    });
+  let finalData = data.map(row => {
+    return row.reduce((result, field, index) => {
+      result[colums[index]] = row[index];
+      return result;
+    }, {})
+  });
 
-    res.json(finalData) 
+  res.json(finalData)
 })
-
-console.log("server starting..");
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
